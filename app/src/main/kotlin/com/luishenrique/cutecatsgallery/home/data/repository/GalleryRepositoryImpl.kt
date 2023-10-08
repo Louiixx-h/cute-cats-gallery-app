aee128efa12f440728a851e2eeed531d92e38572
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-10-08 15:09:14 -0300

    refactor: renamed HomeApi

--- app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/data/repository/GalleryRepositoryImpl.kt
+++ app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/data/repository/GalleryRepositoryImpl.kt
@@ -1,10 +1,10 @@
 package com.luishenrique.cutecatsgallery.home.data.repository
 
 import com.luishenrique.cutecatsgallery.home.data.mapper.GalleryMapper
-import com.luishenrique.cutecatsgallery.home.data.network.ApiService
+import com.luishenrique.cutecatsgallery.home.data.network.HomeApi
 import com.luishenrique.cutecatsgallery.home.domain.model.Gallery
 
-class GalleryRepositoryImpl(private val service: ApiService) : GalleryRepository {
+class GalleryRepositoryImpl(private val service: HomeApi) : GalleryRepository {
 
     override suspend fun findAllCats(page: Int): Gallery {
         return GalleryMapper.mapToModel(service.finAllCats(page = page))
