2554324cdc683d5879421673a22b8f6ac9d05945
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-10-08 13:38:16 -0300

    refactor: created response model

--- app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/data/network/response/GalleryResponse.kt
+++ app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/data/network/response/GalleryResponse.kt
@@ -2,52 +2,6 @@ package com.luishenrique.cutecatsgallery.home.data.network.response
 
 import com.google.gson.annotations.SerializedName
 
-class Gallery(
-    @SerializedName("data") val data: List<Image>
-) {
-    fun filterToPhoto(): List<Image> {
-        val photosCat: MutableList<Image> = mutableListOf()
-        var lastName = ""
-        var currentName = ""
-        data.forEach { image ->
-            currentName = image.username
-            image.images?.forEach {
-                if (it.type == "image/jpeg" && currentName != lastName) {
-                    lastName = currentName
-                    photosCat.add(image)
-                }
-            }
-        }
-        return photosCat
-    }
-
-    fun filterToVideo(): List<Image> {
-        val videosCat: MutableList<Image> = mutableListOf()
-        var lastName = ""
-        var currentName = ""
-        data.forEach { image ->
-            currentName = image.username
-            image.images?.forEach {
-                if (it.type == "image/mp4" && currentName != lastName) {
-                    lastName = currentName
-                    videosCat.add(image)
-                }
-            }
-        }
-        return videosCat
-    }
-}
-
-class Image(
-    @SerializedName("title") val title: String,
-    @SerializedName("account_url") val username: String,
-    @SerializedName("score") val score: Int,
-    @SerializedName("images") val images: List<ImageInfo>?
-)
-
-class ImageInfo(
-    @SerializedName("type") val type: String,
-    @SerializedName("link") val link: String,
-    @SerializedName("gifv") val gifv: String,
-    @SerializedName("mp4") val mp4: String,
+data class GalleryResponse(
+    @SerializedName("data") val data: List<CardImageResponse?>?
 )
