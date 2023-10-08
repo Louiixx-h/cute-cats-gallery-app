c74625f40c60befeb9714879273eab16d6a77ef9
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-10-08 14:24:49 -0300

    feat: modules added on startKoint

--- app/src/main/kotlin/com/luishenrique/cutecatsgallery/App.kt
+++ app/src/main/kotlin/com/luishenrique/cutecatsgallery/App.kt
@@ -1,6 +1,7 @@
 package com.luishenrique.cutecatsgallery
 
 import android.app.Application
+import com.luishenrique.cutecatsgallery.di.DependencyInjection
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -11,7 +12,7 @@ class App : Application() {
 
         startKoin {
             androidContext(this@App)
-            modules()
+            modules(DependencyInjection.modules)
         }
     }
 }
