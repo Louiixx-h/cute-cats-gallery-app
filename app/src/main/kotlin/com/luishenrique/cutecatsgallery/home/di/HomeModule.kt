e2650bddbd5d82f2b2928eb4f82fee431a41f1d8
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-10-08 15:09:04 -0300

    refactor: added HomeApi injection

--- app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/di/HomeModule.kt
+++ app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/di/HomeModule.kt
@@ -1,6 +1,6 @@
 package com.luishenrique.cutecatsgallery.home.di
 
-import com.luishenrique.cutecatsgallery.BuildConfig
+import com.luishenrique.cutecatsgallery.home.data.network.HomeApi
 import com.luishenrique.cutecatsgallery.home.data.repository.GalleryRepository
 import com.luishenrique.cutecatsgallery.home.data.repository.GalleryRepositoryImpl
 import com.luishenrique.cutecatsgallery.home.domain.GalleryUseCase
@@ -9,6 +9,7 @@ import com.luishenrique.cutecatsgallery.home.presentation.HomeActivity
 import com.luishenrique.cutecatsgallery.home.presentation.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.scopedOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -16,13 +17,14 @@ import retrofit2.Retrofit
 object HomeModule {
     val instance = module {
         scope<HomeActivity> {
+            scopedOf(::provideHomeApi)
             factoryOf(::GalleryRepositoryImpl) bind GalleryRepository::class
             factoryOf(::GalleryUseCaseImpl) bind GalleryUseCase::class
             viewModelOf(::HomeViewModel)
         }
     }
 
-    fun provideRetrofit() : Retrofit {
-        return Retrofit.Builder().baseUrl(BuildConfig.API_URL)
+    private fun provideHomeApi(retrofit: Retrofit): HomeApi {
+        return retrofit.create(HomeApi::class.java)
     }
 }
