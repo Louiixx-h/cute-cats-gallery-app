4b1ce7fdebf3aededba6744d76168ba2b5155812
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-10-08 13:39:05 -0300

    refactor: created gallery resource

--- app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/presentation/GalleryResource.kt
+++ app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/presentation/GalleryResource.kt
@@ -1,6 +1,6 @@
 package com.luishenrique.cutecatsgallery.home.presentation
 
-sealed class Result<out R> {
-    data class Success<out T>(val data: T?) : Result<T?>()
-    data class Error(val exception: Exception) : Result<Nothing>()
+sealed class GalleryResource<out R> {
+    data class Success<out T>(val data: T?) : GalleryResource<T?>()
+    data class Error(val exception: Exception) : GalleryResource<Nothing>()
 }
