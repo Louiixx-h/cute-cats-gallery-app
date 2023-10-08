c23d554da1cb1dc469e29e496b6afcad6c79e715
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-10-08 14:24:31 -0300

    feat: removed AppModule.kt

--- app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/di/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package com.luishenrique.cutecatsgallery.home.di
-
-import com.luishenrique.cutecatsgallery.home.presentation.HomeViewModel
-import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val viewModelModule = module {
-    viewModel { HomeViewModel(useCase = get()) }
-}
