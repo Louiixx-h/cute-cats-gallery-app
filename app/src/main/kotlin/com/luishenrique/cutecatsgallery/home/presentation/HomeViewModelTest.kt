60175b9cd2fe3d0ce586e95042a5503feed105e2
Luis Henrique <65178969+Louiixx-h@users.noreply.github.com>
2023-11-01 12:43:25 -0300

    update

---
+++ app/src/main/kotlin/com/luishenrique/cutecatsgallery/home/presentation/HomeViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.luishenrique.cutecatsgallery.home.presentation
+
+import org.junit.Before
+import org.junit.Test
+
+class HomeViewModelTest {
+
+//    private lateinit var viewModel: HomeViewModel
+
+    @Before
+    fun before() {
+//        viewModel = null
+    }
+
+//    @Test
+//    fun `given getCats method when call it then cancel job`() {
+////        viewModel.getCats()
+//    }
+}
